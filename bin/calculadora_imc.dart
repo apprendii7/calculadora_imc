f9c31b35075b086fc55f1874d934b25b8a2695b6
Alexandre Assunção <apprendiiz@yahoo.com.br>
2023-10-21 12:25:17 -0300

    feat: ok

--- bin/calculadora_imc.dart
+++ bin/calculadora_imc.dart
@@ -1,5 +1,25 @@
 import 'package:calculadora_imc/calculadora_imc.dart' as calculadora_imc;
+import 'package:calculadora_imc/classes/pessoa.dart';
+import 'package:test/test.dart';
 
 void main(List<String> arguments) {
-  print('Hello world: ${calculadora_imc.calculate()}!');
+  var pessoa = Pessoa();
+  try {
+    print('Calculadora IMC');
+    print('Digite seu nome:');
+    pessoa.setNome(calculadora_imc.lerString());
+
+    print('Digite sua altura:');
+    pessoa.setAltura(calculadora_imc.lerDouble());
+
+    print('Digite seu peso:');
+    pessoa.setPeso(calculadora_imc.lerDouble());
+
+    double imcValue = calculadora_imc.imc(pessoa.getPeso(), pessoa.getAltura());
+    String mensagemImc = calculadora_imc.mensagemImc(imcValue);
+
+    print('Olá ' + pessoa.getNome() + ', Seu IMC é $imcValue, $mensagemImc');
+  } catch (erro) {
+    print('Ocorreu um erro, execute a aplicação novamente');
+  }
 }
--- test/calculadora_imc_test.dart
+++ test/calculadora_imc_test.dart
@@ -2,7 +2,11 @@ import 'package:calculadora_imc/calculadora_imc.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
+  test('imc', () {
+    expect(imc(100, 1.74).truncate(), 33);
+  });
+
+  test('mensagemImc', () {
+    expect(mensagemImc(33), "Obesidade grau I");
   });
 }
